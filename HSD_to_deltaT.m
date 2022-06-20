b7cfa05d7590e1ecbb6a1e24fe6289bc0902a301
HRNanoOpics <79983709+HRNanoOpics@users.noreply.github.com>
2022-06-20 21:39:43 +0200

    Update HSD_to_deltaT.m

--- HSD_to_deltaT.m
+++ HSD_to_deltaT.m
@@ -1,6 +1,10 @@
 function [delta_T_map]=HSD_to_deltaT(HSD,green_T)
 
    %Convolution 3D calculation with Fourier transform
+   % Code used in article entitled "Thermal Shaping of Arbitrary Optical Wavefronts using Light Patterning" 
+   % from Hadrien M.L. Robert, Martin Cicala and Marek Piliarik*, 
+   %Institute of Photonics and Electronics of the Czech Academy of Sciences, Chaberská 1014/57, 18251 Prague, Czech Republic. *piliarik@ufe.cz  
+
    
     HSD_F=fftn(fftshift(HSD));
     Green_T_F=fftn(fftshift(green_T));
